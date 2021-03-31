9cce591fb8347991423518b427888b59ccda7dbe
Harsh-jot <72195283+Harsh-jot@users.noreply.github.com>
2021-04-01 00:51:13 +0530

    Update translator.py

--- translator.py
+++ translator.py
@@ -1,8 +1,9 @@
-//An application to help translate between languages
+#An application to help translate between languages
 
 from googletrans import Translator statement=str(input("Statement:")); 
-#translator translator=Translator(); 
+#translator 
+translator=Translator(); 
 #translating the statement 
-translated_statement=translator.translate(statement,src='en',dest='language in whic hyou want to translate the statement'); 
+translated_statement=translator.translate(statement,src='en',dest='language in which you want to translate the statement'); 
 #to print the 
 translated statement print(translated_statement.text);
